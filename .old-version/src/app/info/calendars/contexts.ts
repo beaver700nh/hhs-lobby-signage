70acc0c80cc5c51ee771dc58b19740cfa1fbfd1b
Minh Le <41931581+beaver700nh@users.noreply.github.com>
2025-06-05 01:24:37 +0000

    Set up a new project using React + Vite

--- .old-version/src/app/info/calendars/contexts.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import ical from "node-ical";
-import { Calendars } from "./definitions";
-
-export type ContextType = Promise<ical.VEvent | null>;
-
-export const Contexts = new Map(Calendars.map(
-  ({ name }) => [ name, createContext<ContextType>(new Promise(() => {})) ]
-));
--- .old-version/src/app/info/header/big-letter/loading.tsx
+++
@@ -1,7 +0,0 @@
-import Skeleton from "@mui/material/Skeleton";
-
-export default function BigLetterLoading() {
-  return (
-    <Skeleton variant="rounded" width="100%" height="100%" />
-  );
-}
